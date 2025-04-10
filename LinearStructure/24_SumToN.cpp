ca07ab2e41d0f2076a989afdde985994e37544db
FFFF-0000h <adebiyiitunuayo@gmail.com>
2025-04-10 07:51:22 +0100

    Add 24_SumToN.cpp

---
+++ LinearStructure/24_SumToN.cpp
@@ -0,0 +1,26 @@
+/*
+ *
+ 	Determine the value of the sum S = 1+2+3+ ... + n, for >n< a natural number read from the keyboard
+	
+	A natural number is any positive whole number you can count with, starting from 1. Usually non-zero, non-negative, non-decimal.
+	So natural numbers are:
+1, 2, 3, 4, 5, 6, …
+
+	Using the formula known as the Gauss Summation Formula or Arithmetic Series Formula (for the special case where the first term is 1 and the common difference is 1).
+		S=(n(n + 1)) / 2
+
+ *
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n,sumN;
+	cout<<"Input a natural number: ";
+	cin>>n;
+	sumN = (n * (n + 1)) / 2;
+	cout<<"The sum of the 'n' natural number(s) is: "<<sumN<<endl;
+	return 0;
+}
