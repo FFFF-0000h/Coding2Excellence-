4045a0e6e6a7a4faea8456123a17282cde470059
FFFF-0000h <adebiyiitunuayo@gmail.com>
2025-04-12 11:04:44 +0100

    Complete code logic: I used the abs() function to be able to determine the magnitude of the difference of any two values and not care about the direction, i.e I do not want the output to be negative as the instruction says to find the >largest difference between any two values< which is the size or magnitude, not whether it is positive or negative as that would be a consideration of direction, which I am not told to do

--- ConditionalStructure/30_Largest_Difference_Between_Three_Values.cpp
+++ ConditionalStructure/30_Largest_Difference_Between_Three_Values.cpp
@@ -10,22 +10,35 @@ using namespace std;
 
 int main()
 {
-	int a,b,c,largestOne,largestTwo;
+	int a,b,c,diff1,diff2,diff3,largestOne,largestTwo;
 	cout<<"Input first number: ";
 	cin>>a;
 	cout<<"Input second number: ";
         cin>>b;
 	cout<<"Input third number: ";
         cin>>c;
-	diff1 = a - b;
-	diff2 = b - c;
-	diff3 = a - c;
-	if (a - b > b - c)
+	diff1 = abs(a - b);
+	diff2 = abs(b - c);
+	diff3 = abs(a - c);
+
+	if (diff1 > diff2)
 	{
-		largestOne = a - b;
+		largestOne = diff1;
 	} else {
-		largestOne = b - c;
+		largestOne = diff2;
 	}
-	cout<<"The largest difference between any two values is: "<<endl;
+
+	if (largestOne > diff3)
+	{
+		largestTwo = largestOne;
+	} else {
+		largestTwo = diff3;
+	}
+
+	// We can as well try the max() function
+        //diffy = max(max(diff1, diff2), diff3) to make the code concise
+        //max() allows us to ditch the multiple conditional if statements
+
+	cout<<"The largest difference between any two values is: "<<largestTwo<<endl;
 	return 0;
 }
