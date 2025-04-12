03cd6e6cdf849e2a0f074fcb2515f9c65258e3bf
FFFF-0000h <adebiyiitunuayo@gmail.com>
2025-04-12 11:17:35 +0100

    Add 31_LargestNumberFromDigits.cpp

---
+++ ConditionalStructure/31_LargestNumberFromDigits.cpp
@@ -0,0 +1,20 @@
+/*
+ *
+ 	A natural number x with exactly 3 digits is read. Generate the largest number that has the same digits as x
+
+	How I think I should solve this problem is this:
+	- Read the 3 digit number and break it down into hundredths, tenths and units form
+	- Then find the largest of the three numbers and store it
+	- Then find the largest of the last two numbers and store it, and store the last digit
+	- Then rebuild the stored digits from largest to lowest
+
+ *
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x,y,z,largest
+}
